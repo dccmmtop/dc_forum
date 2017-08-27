152224b6c78c8b2eb9efd7b1819685e6d9ba57bd
dccmmtop <dccmmtop@gmail.com>
2017-08-27 20:04:21 +0800

    移除没用的关系

--- db/migrate/20170823132023_change_tag.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTag < ActiveRecord::Migration[5.1]
-  def change
-    change_column :tags,:topics_count,:integer,default:0
-  end
-end
--- db/migrate/20170827115238_remove_topics_count_from_tags.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTopicsCountFromTags < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :tags,:topics_count,:integer
-  end
-end
