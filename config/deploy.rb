9e247d089562aaebd2bd9007027e1464ff6267da
dccmmtop <dccmmtop@gmail.com>
2017-11-14 14:28:34 +0800

    mina

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,36 +15,36 @@ set :shared_files, ['config/database.yml', 'config/secrets.yml']
 set :rvm_use_path, '/usr/local/rvm/scripts/rvm'
 desc 'Set up environment.'
 task :remote_environment do
-  invoke :'rvm:use', 'ruby-2.3.3@rails5'
-   # command %[ export PATH="$PATH:$HOME/.rvm/scripts/rvm" ]
+	invoke :'rvm:use', 'ruby-2.3.3@rails5'
+	# command %[ export PATH="$PATH:$HOME/.rvm/scripts/rvm" ]
 end
 desc 'Prepare for deployment.'
 task :setup do
-  task :setup do
-    ['log', 'config', 'public/upload', 'tmp/pids', 'tmp/sockets'].each do |dir|
-      command %{mkdir -p "fetch(:deploy_to)/shared/#{dir}"}
-      command %{chmod g+rx,u+rwx "fetch(:deploy_to)/shared/#{dir}"}
-    end
+	task :setup do
+		['log', 'config', 'public/upload', 'tmp/pids', 'tmp/sockets'].each do |dir|
+			command %{mkdir -p "fetch(:deploy_to)/shared/#{dir}"}
+			command %{chmod g+rx,u+rwx "fetch(:deploy_to)/shared/#{dir}"}
+		end
 
-    ['config/database.yml', 'config/secrets.yml', 'config/puma.rb'].each do |file|
-      command %{touch "fetch(:deploy_to)/shared/#{file}"}
-      comment %{Be sure to edit 'shared/#{file}'.}
-    end
-  end
+		['config/database.yml', 'config/secrets.yml', 'config/puma.rb'].each do |file|
+			command %{touch "fetch(:deploy_to)/shared/#{file}"}
+			comment %{Be sure to edit 'shared/#{file}'.}
+		end
+	end
 end
 desc 'Deploy current version to the server.'
-task :deploy do
-  deploy do
-    invoke :'git:clone'
-    invoke :'deploy:link_shared_paths'
-    invoke :'bundle:install'
-    invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
-    invoke :'deploy:cleanup'
+task deploy: :remote_environment do
+	deploy do
+		invoke :'git:clone'
+		invoke :'deploy:link_shared_paths'
+		invoke :'bundle:install'
+		invoke :'rails:db_migrate'
+		invoke :'rails:assets_precompile'
+		invoke :'deploy:cleanup'
 
-    on :launch do
-      invoke :'puma:start'
-      invoke :'puma:phased_restart'
-    end
-  end
+		on :launch do
+			invoke :'puma:start'
+			invoke :'puma:phased_restart'
+		end
+	end
 end
