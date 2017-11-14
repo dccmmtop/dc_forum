07524d2cb1a40c01b3e14c71c084d57763b62246
dccmmtop <dccmmtop@gmail.com>
2017-11-14 10:21:40 +0800

    add shared

--- Gemfile
+++ Gemfile
@@ -70,6 +70,9 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'mina', group: :development
+  gem 'mina-puma', require: false
+  gem 'mina-logs', require: false
 end
 
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,88 +1,37 @@
+require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
-require 'mina/rvm'    # for rvm support. (https://rvm.io)
-
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
-
-set :application_name, 'dc_forum'
+require 'mina/rvm'
+require 'mina/puma'
+require 'mina/logs'
 set :domain, '39.108.138.149'
-set :deploy_to, '/var/www/dc_forum'
-set :repository, 'https://github.com/dccmmtop/dc_forum.git'
+set :user, 'root'
+set :deploy_to, '/home/deploy/dc_forum'
+set :repository, 'https://github.com/dccmmtop/dc_forum.git'    # private repository
 set :branch, 'master'
 
-# Optional settings:
-set :user, 'root'          # Username in the server to SSH to.
-set :port, '22'           # SSH port number.
-set :forward_agent, true     # SSH forward_agent.
+set :shared_dirs, ['log']
+set :shared_files, ['config/database.yml', 'config/secrets.yml']
 
-# Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
-# Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
-# run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-# set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
-# set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# This task is the environment that is loaded for all remote run commands, such as
-# `mina deploy` or `mina rake`.
-task :remote_environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use', 'ruby-1.9.3-p125@default'
+desc 'Set up environment.'
+task :environment do
+  invoke :'rvm:use', 'ruby-2.3.3@rails5'
 end
-
-# Put any custom commands you need to run at setup
-# All paths in `shared_dirs` and `shared_paths` will be created on their own.
+desc 'Prepare for deployment.'
 task :setup do
-  # 在服务器项目目录的shared中创建log文件夹
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
+  ['log', 'config', 'public/upload', 'tmp/pids', 'tmp/sockets'].each do |dir|
+    command %{mkdir -p "fetch(:deploy_to)/shared/#{dir}"}
+    command %{chmod g+rx,u+rwx "fetch(:deploy_to)/shared/#{dir}"}
+  end
 
-  # 在服务器项目目录的shared中创建config文件夹 下同
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
-
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
-  queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
-
-  # puma.rb 配置puma必须得文件夹及文件
-  queue! %[mkdir -p "#{deploy_to}/shared/tmp/pids"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp/pids"]
-
-  queue! %[mkdir -p "#{deploy_to}/shared/tmp/sockets"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/tmp/sockets"]
-
-  queue! %[touch "#{deploy_to}/shared/config/puma.rb"]
-  queue  %[echo "-----> Be sure to edit 'shared/config/puma.rb'."]
-
-  # tmp/sockets/puma.state
-  queue! %[touch "#{deploy_to}/shared/tmp/sockets/puma.state"]
-  queue  %[echo "-----> Be sure to edit 'shared/tmp/sockets/puma.state'."]
-
-  # log/puma.stdout.log
-  queue! %[touch "#{deploy_to}/shared/log/puma.stdout.log"]
-  queue  %[echo "-----> Be sure to edit 'shared/log/puma.stdout.log'."]
-
-  # log/puma.stdout.log
-  queue! %[touch "#{deploy_to}/shared/log/puma.stderr.log"]
-  queue  %[echo "-----> Be sure to edit 'shared/log/puma.stderr.log'."]
-
-  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml'."]
+  ['config/database.yml', 'config/secrets.yml', 'config/puma.rb'].each do |file|
+    command %{touch "fetch(deploy_to)/shared/#{file}"}
+    comment %{Be sure to edit 'shared/#{file}'.}
+  end
 end
-
-desc "Deploys the current version to the server."
-task :deploy do
-  # uncomment this line to make sure you pushed your local branch to the remote origin
-  # invoke :'git:ensure_pushed'
+desc 'Deploy current version to the server.'
+task :deploy => :environment do
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
@@ -91,17 +40,8 @@ task :deploy do
     invoke :'deploy:cleanup'
 
     on :launch do
-      in_path(fetch(:current_path)) do
-        command %{mkdir -p tmp/}
-        command %{touch tmp/restart.txt}
-      end
+      invoke :'puma:start'
+      invoke :'puma:phased_restart'
     end
   end
-
-  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
-  # run(:local){ say 'done' }
 end
-
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - https://github.com/mina-deploy/mina/tree/master/docs
