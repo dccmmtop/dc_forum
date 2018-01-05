7382869fb8243a855defa887aa5945557a5c65e6
dccmmtop@gmail.com <dccmmtop@gmail.com>
2018-01-06 04:52:57 +0800

    修复公告bug

--- app/helpers/notices_helper.rb
+++ app/helpers/notices_helper.rb
@@ -1,11 +1,15 @@
 module NoticesHelper
+  def notice_count
+    Notice.count 
+  end
+
   def to_me
     if current_user
-    notices = Notice.where("to_user = ?",current_user.email)
-    if notices.size > 0
-      return notices.order(created_at: :desc).first
+      notices = Notice.where("to_user = ?",current_user.email)
+      if notices.size > 0
+        return notices.order(created_at: :desc).first
+      end
     end
-  end
     Notice.order(created_at: :desc).each do |notice|
       next if current_user && notice.to_user && notice.to_user != current_user.email
       return notice
